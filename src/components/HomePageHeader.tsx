02c6f2992894953359a6d22bb62999ee203dacae
ml6ov <karin.mike.2006@au.com>
2026-03-14 01:33:43 +0900

    ホームのヘッダーをHomePageHeaderとしてコンポーネント化

---
+++ src/components/HomePageHeader.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+import decor from "../assets/decor.png"
+
+type Props = {
+  username: string
+}
+
+export default function HomePageHeader({ username }: Props) {
+
+  const [icon, setIcon] = useState<string | null>(null)
+
+  const handleIconChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+
+    if (file) {
+      setIcon(URL.createObjectURL(file))
+    }
+  }
+
+  return (
+    <div>
+
+      {/* ピンクヘッダー */}
+      <div className="header"></div>
+
+      {/* デコレーション */}
+      <img src={decor} className="decor" />
+
+      {/* プロフィール */}
+      <div className="profile">
+
+        <label className="icon-label">
+
+          <input
+            type="file"
+            className="icon-input"
+            onChange={handleIconChange}
+          />
+
+          <img
+            src={icon ?? "https://placehold.jp/150x150.png"}
+            className="icon"
+          />
+
+        </label>
+
+        <h2 className="username">
+          {username}
+        </h2>
+
+      </div>
+
+    </div>
+  )
+}

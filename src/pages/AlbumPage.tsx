e583afe30ba2386c57bdfa6af46bccaaf1aa2e13
ml6ov <karin.mike.2006@au.com>
2026-03-14 23:53:30 +0900

    アルバムの新規作成画面に画面遷移できるようにした

--- src/pages/AlbumPage.tsx
+++ src/pages/AlbumPage.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
-import { FiPlus, FiEdit2, FiTrash2 } from "react-icons/fi"
+import { useState, useEffect } from "react"
+import { useNavigate, useLocation } from "react-router-dom"
+import { FiEdit2, FiTrash2 } from "react-icons/fi"
 import { FaHeart } from "react-icons/fa"
 
 import AppHeader from "../components/AppHeader"
@@ -8,90 +9,131 @@ import TabBar from "../components/TabBar"
 import "../styles/AlbumPage.css"
 
 type Album = {
-  id: number
-  name: string
-  image: string
-  author: string
-  icon: string
-  date: string
+  id:number
+  name:string
+  image:string
+  author:string
+  icon:string
+  date:string
 }
 
-export default function AlbumPage() {
+export default function AlbumPage(){
 
-  const [albums, setAlbums] = useState<Album[]>([
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const [albums,setAlbums] = useState<Album[]>([
     {
-      id: 1,
-      name: "初詣",
-      image: "https://placehold.jp/600x400.png",
-      author: "彼女ちゃん",
-      icon: "https://placehold.jp/40x40.png",
-      date: "2026年1月1日"
+      id:1,
+      name:"初詣",
+      image:"https://placehold.jp/600x400.png",
+      author:"彼女ちゃん",
+      icon:"https://placehold.jp/40x40.png",
+      date:"2026年1月1日"
     }
   ])
 
-  const createAlbum = () => {
 
-    const newAlbum: Album = {
-      id: Date.now(),
-      name: "新しいアルバム",
-      image: "https://placehold.jp/600x400.png",
-      author: "彼女ちゃん",
-      icon: "https://placehold.jp/40x40.png",
-      date: new Date().toLocaleDateString("ja-JP")
+  // ===== AlbumEditから戻ってきたアルバムを追加 =====
+  useEffect(()=>{
+
+    if(location.state?.newAlbum){
+
+      setAlbums((prev)=>[location.state.newAlbum,...prev])
+
     }
 
-    setAlbums([newAlbum, ...albums])
+  },[location.state])
+
+
+  const goCreate = ()=>{
+
+    navigate("/album-new-create")
+
   }
 
-  return (
+
+  const deleteAlbum = (id:number)=>{
+
+    const newList = albums.filter(a=>a.id!==id)
+
+    setAlbums(newList)
+
+  }
+
+
+  return(
 
     <div className="album-page">
 
-      {/* ===== ヘッダー ===== */}
       <AppHeader
         variant="simple"
         title=""
       />
 
+
       {/* 新規作成ボタン */}
-      <button className="album-create-btn" onClick={createAlbum}>
+      <button
+        className="album-create-btn"
+        onClick={goCreate}
+      >
+
         <FaHeart className="heart-icon"/>
-        <FiPlus className="plus-icon"/>
+        <span className="plus-icon">+</span>
+
       </button>
 
 
-      {/* ===== アルバム一覧 ===== */}
+      {/* アルバム一覧 */}
       <div className="album-list">
 
-        {albums.map((album) => (
+        {albums.map((album)=>(
 
           <div key={album.id} className="album-item">
 
-            {/* 投稿者 */}
             <div className="album-user">
 
               <div className="album-user-left">
-                <img src={album.icon} className="album-user-icon" />
-                <span className="album-user-name">{album.author}</span>
+
+                <img
+                  src={album.icon}
+                  className="album-user-icon"
+                />
+
+                <span className="album-user-name">
+                  {album.author}
+                </span>
+
               </div>
 
-              <span className="album-date">{album.date}</span>
+              <span className="album-date">
+                {album.date}
+              </span>
 
             </div>
 
 
-            {/* メイン画像 */}
-            <img src={album.image} className="album-image" />
+            <img
+              src={album.image}
+              className="album-image"
+            />
 
 
-            {/* 下部 */}
             <div className="album-bottom">
 
-              <span className="album-title">{album.name}</span>
+              <span className="album-title">
+                {album.name}
+              </span>
 
               <div className="album-actions">
-                <FiEdit2 className="album-icon-btn" />
-                <FiTrash2 className="album-icon-btn" />
+
+                <FiEdit2 className="album-icon-btn"/>
+
+                <FiTrash2
+                  className="album-icon-btn"
+                  onClick={()=>deleteAlbum(album.id)}
+                />
+
               </div>
 
             </div>
@@ -102,9 +144,10 @@ export default function AlbumPage() {
 
       </div>
 
-
-      <TabBar />
+      <TabBar/>
 
     </div>
+
   )
+
 }
